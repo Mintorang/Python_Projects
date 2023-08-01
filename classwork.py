01ab370d50d50221aa6c99c246ff7b4bf3e58747
Mintorang <125573100+Mintorang@users.noreply.github.com>
2023-08-01 12:36:14 +0100

    Create classwork.py

---
+++ classwork.py
@@ -0,0 +1,40 @@
+list = ["moyo", "zachary", "daniel", "joshua", "max", "arthur", "george", "kaiden", "shrey", "micheal"]
+accepted = []
+counter = 0
+while True:
+    ask = input("State your name:  ")
+    ask_2 = input("Yes or No if you are coming to the party:  ")
+    contact = input("WHat is your number?  ")
+    address = input("What is your PostCode?  ")
+    country = input("WHat country do you live in?  ")
+    city = input("What city/state do you live in?  ")
+    email = input("WHat is your email?  ")
+    ask = ask.lower()
+    ask_2 = ask_2.lower()
+    for i in range(10):
+
+        guest = {
+            "name": ask,
+            "number": contact,
+            "city/state": city,
+            "country": country,
+            "address": address,
+            "party": ask_2,
+            "email": email
+
+
+
+        }
+        
+    if ask_2 == "no":
+        print(f"{ask} is not coming to the party.")
+    else:
+        accepted.append(ask)
+        print(f"{ask} is comong to the party")
+        print(accepted)
+    counter += 1
+    if counter == len(list):
+        break    
+    print(f"Here are the people that are not coming to the party:  {accepted}")
+    for key in guest :
+        print(key, '->', guest[key])
