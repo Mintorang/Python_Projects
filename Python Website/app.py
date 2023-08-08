ea5633451e4dc881e7eef36801c77d0b52693ca8
Mintorang <125573100+Mintorang@users.noreply.github.com>
2023-08-08 11:29:33 +0000

    modified:   Python Website/__pycache__/views.cpython-310.pyc 	modified:   Python Website/app.py 	modified:   Python Website/views.py 	renamed:    Python Website/Forms.html -> templates/Forms.html 	renamed:    Python Website/home.html -> templates/home.html 	renamed:    Python Website/index.html -> templates/index.html 	renamed:    Python Website/profile.html -> templates/profile.html 	deleted:    Python Website Using Tutorial/basic.html 	deleted:    Python Website Using Tutorial/html.html 	modified:   Python Website/app.py 	modified:   classwork.py 	deleted:    templates/Forms.html 	deleted:    templates/home.html 	deleted:    templates/index.html 	deleted:    templates/profile.html 	modified:   while_loop.py 	Python Website/templates/

--- Python Website/app.py
+++ Python Website/app.py
@@ -1,13 +1,24 @@
-from flask import Flask
-from views import views
+# from flask import Flask
+# from views import views
+
+# app = Flask(__name__)
+# #this is a blueprint
+# app.register_blueprint(views, url_prefix="/")
+
+
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port=8000)
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
-#this is a blueprint
-app.register_blueprint(views, url_prefix="/")
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+if __name__ == '__main__':
+    app.run(debug=True)
